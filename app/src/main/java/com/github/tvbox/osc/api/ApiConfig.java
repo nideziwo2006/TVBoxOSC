37eb58d265e99fd353bb3d01a06a6196572df74c
nideziwo2006 <109273989+nideziwo2006@users.noreply.github.com>
2022-07-15 12:08:50 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "http//:yxflq.com/hy.txt");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://yxflq.com/hy.txt");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
